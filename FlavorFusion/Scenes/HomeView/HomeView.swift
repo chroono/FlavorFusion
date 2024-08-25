fb94958ab880c34e09342a0fbb167eb7b90b5b90
Emil Piórkowski <e.piorkowski11@gmail.com>
2024-08-25 09:53:43 +0200

    WelcomeScreen

---
+++ FlavorFusion/Scenes/HomeView/HomeView.swift
@@ -0,0 +1,38 @@
+//
+//  HomeView.swift
+//  FlavorFusion
+//
+//  Created by Emil Piórkowski on 25/08/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @AppStorage("firstTime") var firstTime: Bool = true
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .sheet(isPresented: $firstTime) {
+            VStack(alignment: .leading) {
+                Image(systemName: "command.square.fill")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                Text("Flavor Fusion")
+                    .font(.title)
+                    .foregroundStyle(.red)
+                Text("The best dishes in one place")
+                    .font(.title2)
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
