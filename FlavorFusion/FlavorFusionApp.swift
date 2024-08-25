e9d922d91bd332348e6cf7e2762004e5f2aa6060
Emil Piórkowski <e.piorkowski11@gmail.com>
2024-08-25 09:23:03 +0200

    Initial Commit

---
+++ FlavorFusion/FlavorFusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorFusionApp.swift
+//  FlavorFusion
+//
+//  Created by Emil Piórkowski on 25/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlavorFusionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
